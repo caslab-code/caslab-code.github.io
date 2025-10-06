6d300a79df20d29f89beedbfa1188a56065500d5
Jakub Szefer <jakub.szefer@yale.edu>
2025-10-06 14:26:10 -0500

    Adding public helper script for cleaning Bibtex files.

---
+++ public/scripts/bibtex-helper/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+bibtoolrsc=$(cat << 'EOF'
+# Formatting output
+print.align  = on       # align fields for readability
+print.indent = 2        # indent continuation lines
+print.line.length = 512 # set value to be very large to prevent line wrapping
+
+# Sorting
+sort = on
+sort.format = {author}
+
+# Remove unwanted fields (optional – add or remove as needed)
+delete.field {abstract}
+delete.field {file}
+delete.field {keywords}
+delete.field {note}
+delete.field {doi}
+EOF
+)
+
+# bibtool -s -r bibtool.rsc refs.bib -o refs_formatted.bib
+bibtool -s -r <(echo "$bibtoolrsc") refs.bib -o refs_formatted.bib
+
--- public/scripts/clean_bibtex.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bibtool -s -r bibtool.rsc ../refs.bib -o ../refs_formatted.bib
